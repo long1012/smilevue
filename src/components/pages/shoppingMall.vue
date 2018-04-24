0613baec6857cebf7b9eda4c684fc40e3835bb16
long1012 <294946048@qq.com>
2018-04-24 09:56:03 +0800

    首页搜索框布局

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        <van-row class="test-row">
-            <van-col span="8">span:8</van-col>
-            <van-col span="8">span:8</van-col>
-            <van-col span="8">span:8</van-col>
-        </van-row>
+        <div class="search-bar">
+            <van-row class="test-row">
+                <van-col span="3">
+                    <img :src="locationIcon" alt="" width="80%" class="location-icon">
+                </van-col>
+                <van-col span="16">
+                    <input type="text" class="search-input"/>
+                </van-col>
+                <van-col span="5" class="btn-fine">
+                    <van-button size="mini">查找</van-button>
+                </van-col>
+            </van-row>
+        </div>
+        
     </div>
 </template>
 
@@ -12,14 +21,33 @@
     export default {
         data() {
             return {
-                msg: 'shopping Mal'
+                msg: 'shopping Mal',
+                locationIcon:require('../../assets/images/location.png')
             }
         },
     }
 </script>
 
 <style scoped>
-    .test-row{
+    .search-bar{
+        height:2.2rem;
+        background-color: #e5017d;
+        line-height: 2.2rem;
+    }
+    .search-input{
+        width:100%;
+        height: 1.3rem;
+        border:0;
+        border-bottom: 1px solid #fff !important;
+        background: #e5017d;
+        color:#fff;
+    }
+    .location-icon{
+        padding-left: .3rem;
+        padding-top:.15rem;
+
+    }
+    .btn-fine{
         text-align: center;
     }
 </style>
