af5feb9a9582e7975d9d1e600c43a98d9d2d6365
long1012 <294946048@qq.com>
2018-05-26 11:37:09 +0800

    封装楼层组件

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -48,26 +48,7 @@
                 </swiper>
             </div>
         </div>
-        <div class="floor">
-            <div class="floor-anomaly">
-                <div class="floor-one">
-                    <img :src="floor1_0.image" width="100%" alt="">
-                </div>
-                <div>
-                    <div class="floor-two">
-                        <img :src="floor1_1.image" width="100%" alt="">
-                    </div>
-                    <div>
-                        <img :src="floor1_2.image" width="100%" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="floor-rule">
-                <div v-for="(item,index) in floor1.slice(3)" :key="index">
-                    <img :src="item.image" width="100%" alt="" />
-                </div>
-            </div>
-        </div>
+        <Floor :floorData="floor1"></Floor>
     </div>
 </template>
 
@@ -75,6 +56,7 @@
     import axios from 'axios'
     import 'swiper/dist/css/swiper.css'
     import {swiper,swiperSlide} from 'vue-awesome-swiper'
+    import Floor from '../component/floor.vue'
     export default {
         data() {
             return {
@@ -94,7 +76,8 @@
         },
         components:{
             swiper,
-            swiperSlide
+            swiperSlide,
+            Floor
         },
         created(){
             axios({
@@ -182,36 +165,5 @@
         font-size:12px;
         text-align: center;
     }
-    .floor-anomaly{
-        display:flex;
-        flex-direction: row;
-        background-color: #fff;
-        border-bottom: 1px solid #ddd;
-    }
-    .floor-anomaly div{
-        width:10rem;
-        box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-    }
-    .floor-one{
-        border-right:1px solid #ddd;
-    }
-    .floor-two{
-        border-bottom:1px solid #ddd;
-    }
-    .floor-rule{
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        background-color: #fff;
-    }
-    .floor-rule div{
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-        width:10rem;
-        border-bottom: 1px solid #ddd;
-    }
-    .floor-rule div:nth-child(odd){
-        border-right: 1px solid #ddd;
-    }
+    
 </style>
