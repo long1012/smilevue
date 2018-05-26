d3595748088714d46c6c8f36ce60ebd1c47e16c4
long1012 <294946048@qq.com>
2018-05-26 11:12:10 +0800

    楼层布局

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -48,6 +48,26 @@
                 </swiper>
             </div>
         </div>
+        <div class="floor">
+            <div class="floor-anomaly">
+                <div class="floor-one">
+                    <img :src="floor1_0.image" width="100%" alt="">
+                </div>
+                <div>
+                    <div class="floor-two">
+                        <img :src="floor1_1.image" width="100%" alt="">
+                    </div>
+                    <div>
+                        <img :src="floor1_2.image" width="100%" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="floor-rule">
+                <div v-for="(item,index) in floor1.slice(3)" :key="index">
+                    <img :src="item.image" width="100%" alt="" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -66,6 +86,10 @@
                 category:[],
                 adBanner:'',
                 recommendGoods:[],
+                floor1:[],
+                floor1_0:{},
+                floor1_1:{},
+                floor1_2:{},
             }
         },
         components:{
@@ -84,6 +108,10 @@
                     this.adBanner=res.advertesPicture.PICTURE_ADDRESS
                     this.bannerPic=res.slides;
                     this.recommendGoods=res.recommend;
+                    this.floor1=res.floor1;
+                    this.floor1_0=this.floor1[0];
+                    this.floor1_1=this.floor1[1];
+                    this.floor1_2=this.floor1[2];
                 }
             }).catch(error=>{
                 console.log(error)
@@ -153,5 +181,37 @@
         border-right:1px solid #eee;
         font-size:12px;
         text-align: center;
+    }
+    .floor-anomaly{
+        display:flex;
+        flex-direction: row;
+        background-color: #fff;
+        border-bottom: 1px solid #ddd;
+    }
+    .floor-anomaly div{
+        width:10rem;
+        box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+    }
+    .floor-one{
+        border-right:1px solid #ddd;
+    }
+    .floor-two{
+        border-bottom:1px solid #ddd;
+    }
+    .floor-rule{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        background-color: #fff;
+    }
+    .floor-rule div{
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        width:10rem;
+        border-bottom: 1px solid #ddd;
+    }
+    .floor-rule div:nth-child(odd){
+        border-right: 1px solid #ddd;
     }
 </style>
