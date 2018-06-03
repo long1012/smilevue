f9d2fa9ea06894b887edc52acd1e7470b5a7bb04
long1012 <294946048@qq.com>
2018-06-03 17:05:51 +0800

    安装mongodb，并且使用mongoose链接数据库

--- service/index.js
+++ service/index.js
@@ -1,6 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
+const { connect } = require("./database/init.js")
 
+;(async()=>{
+    await connect();
+})()
 app.use(async (ctx) => {
     ctx.body = '<h1>hello Koa2</h1>'
 })
