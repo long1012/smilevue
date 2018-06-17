91a3f7aa721521e26a53ff99eb16e7c111f5918f
long1012 <294946048@qq.com>
2018-06-17 16:55:58 +0800

    Koa-router，如果是大型项目，可是使用阿里的egg.js

--- service/index.js
+++ service/index.js
@@ -3,11 +3,23 @@ const app = new Koa()
 const { connect, initSchemas } = require("./database/init.js")
 const mongoose = require('mongoose')
 
+const Router = require('koa-router')
+let user = require('./appApi/user.js')
+
+//装在所有子路由
+let router = new Router()
+router.use('/user',user.routes());
+//加载路由中间件
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
 ;(async()=>{
     await connect();
     initSchemas();
     const user=mongoose.model('user');
-    let onUser = new user({userName:'xiaolong2',password:'123456'});
+    let onUser = new user({userName:'xiaolong3',password:'123456'});
     onUser.save().then(()=>{
         console.log('插入成功')
     })
@@ -16,6 +28,6 @@ app.use(async (ctx) => {
     ctx.body = '<h1>hello Koa2</h1>'
 })
 
-app.listen(13000, () => {
+app.listen(3000, () => {
     console.log('node success')
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingMAll from '@/components/pages/shoppingMAll'
-
+import Register from '@/components/pages/register'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'ShoppingMAll',
-      component: ShoppingMAll
-    }
+    {path: '/',name: 'ShoppingMAll',component: ShoppingMAll},
+    {path:'/register',name:'Register',component: Register}
   ]
 })
