13b17b6e5f73c2dec08cf7e53250cc95f610a7c0
long1012 <294946048@qq.com>
2018-08-12 16:16:45 +0800

    商品详情页编写

--- src/components/pages/Goods.vue
+++ src/components/pages/Goods.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
-        商品详情
+        <div class="navbar-div">
+            <van-nav-bar 
+                title="商品详情" 
+                left-text="返回"
+                left-arrow
+                @click-left="onClickLeft"
+            />
+        </div>
+        <div class="topimage-div">
+            <img :src="goodsInfo.IMAGE1" width="100%" alt="" />
+        </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
     import url from '@/serviceAPI.config.js'
+    import {Toast} from 'vant'
     export default {
         data() {
             return {
-                goodsId:''
+                goodsId:'',
+                goodsInfo:{}    //商品详细信息
+
             }
         },
         created() {
@@ -25,10 +38,18 @@
                     method:'post',
                     data:{goodsId:this.goodsId}
                 }).then(response=>{
-                    console.log(response)
+                    if (response.data.code==200 && response.data.message) {
+                        this.goodsInfo = response.data.message
+                    }else{
+                        Toast("服务器错误，数据获取失败")
+                    }
+                    
                 }).catch(error=>{
                     console.log(error)
                 })
+            },
+            onClickLeft(){
+                this.$router.go(-1)
             }
         },
     }
