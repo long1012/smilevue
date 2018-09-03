c6e62654f908f97dc39e388a5c060bd5d8d51d79
long1012 <294946048@qq.com>
2018-09-03 14:26:51 +0800

    建立购物车页面

---
+++ src/components/pages/Cart.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="navvar-div">
+            <van-nav-bar title="购物车" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                cartInfo: [],
+                isEmpty:false
+            }
+        },
+        created(){
+            this.getCartInfo()
+        },
+        methods: {
+            //得到购物车数据的方法
+            getCartInfo() {
+                if(localStorage.cartInfo){
+                    this.cartInfo = JSON.parse(localStorage.cartInfo)
+                }
+                console.log(JSON.stringify(this.cartInfo)+'this.cartInfo')
+                this.isEmpty=this.cartInfo.length > 0 ? ture : false
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
